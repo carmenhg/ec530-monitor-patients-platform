a9e4d295ad19148c89d3f390d9420ee82f1e8c41
Carmen Hurtado <carmenhg@bu.edu>
2022-02-13 14:00:28 -0500

    input data file test

--- test/test_device.py
+++ test/test_device.py
@@ -2,10 +2,10 @@ import device
 
 #this file contains unit test s for the device module 
 def test1_all_data():
-    device.add_data("device_input_data/test1.json")
+    device.add_data("test/device_input_data/test1.json")
     
 def test2_no_did():
-    device.add_data("device_input_data/test2.json")
+    device.add_data("test/device_input_data/test2.json")
 
 def test3_pull_uid():
     device.get_data("1234", "")
