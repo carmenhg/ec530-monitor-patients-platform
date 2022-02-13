f0300fd9058aa20c13439c98d9bbb24f6d96ea5e
Carmen Hurtado <carmenhg@bu.edu>
2022-02-13 13:45:45 -0500

    testing unit test

--- test/test_device.py
+++ test/test_device.py
@@ -2,14 +2,14 @@ import device
 
 #this file contains unit test s for the device module 
 def test1_all_data():
-    add_data("device_input_data/test1.json")
+    device.add_data("device_input_data/test1.json")
     
 def test2_no_did():
-    add_data("device_input_data/test2.json")
+    device.add_data("device_input_data/test2.json")
 
 def test3_pull_uid():
-    get_data("1234", "")
+    device.get_data("1234", "")
 
 def test3_pull_did():
-    get_data("", "abcd")
+    device.get_data("", "abcd")
     
