b2440b69bc541803e63b693e3fc5cc805fea68d4
Carmen Hurtado <carmenhg@bu.edu>
2022-02-15 22:55:14 -0500

    unit tests take 8

--- test/push_data.py
+++ test/push_data.py
@@ -32,34 +32,35 @@ def push_data(json_input):
     #close the file
     input_f.close()
 
-    if(input_data["device_key"] not in regist_devices.values()):
-        print("The device is not registered in our systems. We cannot accept your data. Please register your device")
-    else:
-        #extract needed data: type, measurement, timestamp
-        #check for errors
-        """
-        1. Missing values
-        2. incorrect type (str, float, etc)
-        3. measurement that makes no sense: this would be different for devices of different types. what to do??
-        """
-        for key in to_append:
-            if (input_data[key] == "") or (type(input_data[key]) != str) :
-                print("Value is missing or has incorrect format")
-            else:
-                to_append[key] = input_data[key]
+    if(input_data["device_key"]):
+        if(input_data["device_key"] not in regist_devices.values()):
+            print("The device is not registered in our systems. We cannot accept your data. Please register your device")
+        else:
+            #extract needed data: type, measurement, timestamp
+            #check for errors
+            """
+            1. Missing values
+            2. incorrect type (str, float, etc)
+            3. measurement that makes no sense: this would be different for devices of different types. what to do??
+            """
+            for key in to_append:
+                if (input_data[key] == "") or (type(input_data[key]) != str) :
+                    print("Value is missing or has incorrect format")
+                else:
+                    to_append[key] = input_data[key]
         
-    #finally save the data into our DB(soon to come), json output file for now
-    #open file to read
-    temp_f = open("test/devices_output/devices.json")
-    # returns JSON object as a dictionary
-    temp_data = json.load(temp_f)
-    #close file
-    temp_f.close()
+            #finally save the data into our DB(soon to come), json output file for now
+            #open file to read
+            temp_f = open("test/devices_output/devices.json")
+            # returns JSON object as a dictionary
+            temp_data = json.load(temp_f)
+            #close file
+            temp_f.close()
 
-    temp_data.append(to_append)
+            temp_data.append(to_append)
 
 
-    with open("test/devices_output/devices.json", "r+") as file:
-        json.dump(temp_data, file, indent=4,  separators=(',',': '))
+            with open("test/devices_output/devices.json", "r+") as file:
+                json.dump(temp_data, file, indent=4,  separators=(',',': '))
 
-    file.close()
+            file.close()
