d985043d13bfc30dde6c3b4f4ec08b32bfd9fca6
Carmen Hurtado <carmenhg@bu.edu>
2022-02-21 10:19:38 -0500

    api first unit tests implemented

--- flask-api/test-api/test_app.py
+++ flask-api/test-api/test_app.py
@@ -2,11 +2,15 @@ import requests
 
 Base_URL = "http://127.0.0.1:5000/"
 
+#Unit tests for pulling data 
 get_response = requests.get(Base_URL)
 print(get_response.json())
 
-#api knows which post method to call depending on args parsed???
-post1_response = requests.post(Base_URL, {"device_type": "temp", "device_key": "1234"})
-print(post1_response.json())
+#Unit tests for pushing data
+post_push_response = requests.post(Base_URL, {"device_type": "temp", "device_key": "1234", "device_id": "abcdef", "measuremnet": "100", "timestamp": "16:47"})
+print(post_push_response.json())
 
-#need to add more unit tests 
+#Unit test for registering devices
+#api knows which post method to call depending on args parsed???
+post_register_response = requests.post(Base_URL, {"device_type": "temp", "device_key": "1234"})
+print(post_register_response.json())
