185987a3c73d5d32a775227cdca29f8b93be97cd
Carmen <carmenhg@bu.edu>
2022-03-22 13:21:05 +0000

    update

--- README.md
+++ README.md
@@ -2,10 +2,17 @@
 
 Author: Carmen Hurtado 
 
-## Branching Structure
-* Each module will be implemented in their own branch
-* Main will have completely working code that will ultimately bring together all the modules. 
+## Design and processing criteria:
 
-## Module Documentation
-Please look at individual branches for an overall description of the module. Each module will have separate files for separate functions. These files have documentation on how the interface works and specifics on how each function performs its supposed job. 
-Each function will have, at the beginning of the file, a short summary of what its intended purpose is and what the data input and output look like. 
+1. Simultaneous API calls: I think this would depend on the processing unit of the server
+    - I am hosting my API flask app in an Azure VM. Here I can see the machine’s CPU usage over time
+        - For the CPU to have a maximum efficiency for computations, we want to leave CPU usage to be a maximum of 20% at any given time. 
+        - One request takes about INSERT_NUMBER_HERE percentage of CPU so we can do INSERT_NUMBER_HERE requests at a time. 
+2. Different API calls procedure
+    - Different APIs can run at the same time
+        - The only constraint I put on this is to not have APIs that access the same database entry called at the same time
+            - This can cause errors if they try to write on the same spot
+            - Or if one tries to read an entry that is being modified by the other one. The information would be out of date
+3. Split the processing of an API into multiple threads or processes?
+
+
---
+++ threads.py
@@ -0,0 +1,63 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+
+app = Flask(__name__)
+api = Api(app)
+
+# Parser will make sure that the predetremined parameters for the POST and GET methods are met
+# So all the attributes I need to call the functions will be checked before trying to push the data 
+#the "help" parameter is essentially an error message to the sender 
+
+#args for uploading a message 
+push_args = reqparse.RequestParser()
+push_args.add_argument("sender_id", type=str, help="Must provide sender id", required=True)
+push_args.add_argument("receiver_id", type=str, help="Must provide receiver id", required=True)
+push_args.add_argument("data_string", type=str, help="Must provide text data", required=True)
+push_args.add_argument("data_voice", type=str, help="Must provide text data")
+push_args.add_argument("data_video", type=str, help="Must provide text data")
+push_args.add_argument("timestamp", type=str, help="Must provide timestamp", required=True)
+
+#args for getting messages
+pull_messages_args = reqparse.RequestParser()
+pull_messages_args.add_argument("receiver_id", type=str, help="Must receiver id", required=True)
+
+#args for getting messages by keyword 
+pull_keyword_args = reqparse.RequestParser()
+pull_keyword_args.add_argument("receiver_id", type=str, help="Must provide receiver id", required=True)
+pull_keyword_args.add_argument("keyword", type=str, help="Must provide keyword", required=True)
+
+class UploadMessage(Resource):
+
+    def post(self):
+        args = push_args.parse_args()
+        success, data = upload_message.upload_message(args["sender_id"], args["receiver_id"], args["data_string"], args["data_voice"], args["data_video"], args["timestamp"])
+        if success == True:
+            return data,200
+        elif success == False:
+            return data, 400 
+
+class SearchKeyword(Resource):
+    def get(self):
+        args = pull_keyword_args.parse_args()
+        success, data = search_by_keyword.search_by_keyword(args["receiver_id"], args["keyword"])
+        if success == True:
+            return data,200
+        elif success == False:
+            return data, 400
+
+class RetrieveMessage(Resource):
+    def get(self):
+        args = pull_messages_args.parse_args()
+        success, data = retrieve_message.retrieve_message(args["receiver_id"])
+        if success == True:
+            return data,200
+        elif success == False:
+            return data, 400
+
+api.add_resource(UploadMessage, "/send_message")
+api.add_resource(SearchKeyword, "/search_keyword")
+api.add_resource(RetrieveMessage, "/get_messages")
+
+if __name__=="__main__":
+    app.run(debug=True)
